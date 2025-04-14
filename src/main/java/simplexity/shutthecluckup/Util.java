3968e63d7770781845860adc7c9667e5e11a6dbd
Rhythmic System <mc.adhdmc@gmail.com>
2025-04-14 12:07:39 -0700

    commit

--- src/main/java/simplexity/shutthecluckup/Util.java
+++ src/main/java/simplexity/shutthecluckup/Util.java
@@ -14,6 +14,8 @@ public class Util {
     public static final Permission BYPASS_PERM = new Permission("silence.bypass.mob-type");
     public static final Permission WAND_COMMAND = new Permission("silence.wand");
     public static final Permission WAND_OTHER_COMMAND = new Permission("silence.other.wand");
+    public static final Permission SILENCE_MOBS_COMMAND = new Permission("silence.command");
+    public static final Permission SILENCE_MOBS_COMMAND_RADIUS_OVERRIDE = new Permission("silence.bypass.radius");
 
     public static boolean playerHasEntityPerms(Player player, EntityType entityType) {
         if (player.hasPermission(BYPASS_PERM)) return true;
--- src/main/java/simplexity/shutthecluckup/commands/SilenceMobsCommands.java
+++ src/main/java/simplexity/shutthecluckup/commands/SilenceMobsCommands.java
@@ -1,4 +1,24 @@
 package simplexity.shutthecluckup.commands;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.EntitySelectorArgumentResolver;
+import org.bukkit.entity.Entity;
+import simplexity.shutthecluckup.Util;
+
+import java.util.List;
+
 public class SilenceMobsCommands {
+
+    public static LiteralCommandNode<CommandSourceStack> createCommand() {
+        return Commands.literal("silence-mobs")
+                .requires(sender -> sender.getSender().hasPermission(Util.SILENCE_MOBS_COMMAND))
+                .then(Commands.argument("entity", ArgumentTypes.entity()))
+                    .executes(context -> {
+                        EntitySelectorArgumentResolver entitySelectorArgumentResolver = context.getArgument("entity")
+                        List<Entity> gatheredEntities
+                    })
+    }
 }
